650cc3bcf1bf90db70735b01bed24854df83fb51
KamranYsupov <kamrankek48@gmail.com>
2025-09-18 14:55:10 +0400

    добавил smtp

--- src/config/settings.py
+++ src/config/settings.py
@@ -128,6 +128,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 SMSC_LOGIN = os.getenv('SMSC_LOGIN')
 SMSC_PASSWORD = os.getenv('SMSC_PASSWORD')
 SMSC_SEND_API_URL = 'https://smsc.ru/sys/send.php'
